da703246817fe94bd6975af10bda9d3661902430
Anderson Santos <anderson.santos@eunerd.com.br>
2023-03-23 23:58:44 -0300

    update like

--- src/index.js
+++ src/index.js
@@ -23,17 +23,29 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  console.log(repositories);
+
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
+  }
+
+  const updatedRepository = {
+    id: uuid(),
+    title: title,
+    url: url,
+    techs: techs,
+    likes: 0
   }
 
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
